d42af811dfa27e783b013bd6df248235e0c99174
Anderson Choren <andersonchoren@gmail.com>
2022-02-03 09:33:51 -0300

    Aula 2

---
+++ aula2/src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main(){
+    // Variável
+    var nome:String = "Anderson"
+    var idade:Byte = 30
+    var teste = 14
+
+    nome = "Marco"
+    idade = 20
+    teste = 34
+
+    // Constante
+    val nomeCompleto = "Anderson Choren"
+//    nomeCompleto = "Anderson Coimbra Choren"
+}
